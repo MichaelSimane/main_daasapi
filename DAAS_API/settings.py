9d08930477f46c87b1a3960b41f48a63a2d73e89
MichaelSimane <mickeysimanemb@gmail.com>
2023-08-09 10:28:32 +0300

    Done vercel setting

--- DAAS_API/settings.py
+++ DAAS_API/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url  
+from decouple import config 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 MEDIA_URL = '/Files/'
@@ -97,6 +99,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
