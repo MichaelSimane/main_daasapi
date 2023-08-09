a8fbe1733c28145acf3d8bc7c9ddcc05fa61f660
MichaelSimane <mickeysimanemb@gmail.com>
2023-08-09 10:30:38 +0300

    Done vercel setting

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-echo = "BUILD START"
-pip install -r requirements.txt
-python3.9 manage.py collectstatic 
+echo "BUILD START"
+python3.9 - m pip install -r requirements.txt
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "collecting static"
+python3.9 manage.py collectstatic --noinput --clear 
 echo = "BUILD END"
